dde1ba9e063936cec3abd04e82eca6486a55dc7c
Zachandow <54606352+Zachandow@users.noreply.github.com>
2021-05-07 15:29:09 -0700

    Update address.hpp

--- address.hpp
+++ address.hpp
@@ -1,1 +1,24 @@
 
+#pragma once
+using BYTE = uint8_t;
+using SBYTE = int8_t;
+
+class LogicalAddress {
+private:
+    uint16_t address; 
+
+public:
+
+    LogicalAddress(int loc) {
+        this->address = static_cast<decltype(this->address)>(loc);
+    }
+
+    BYTE getPage() {
+        return (0xFF00 & this->address) >> 8;
+    }
+
+    BYTE getOffset() {
+        return (0xFF & this->address);
+    }
+
+};
